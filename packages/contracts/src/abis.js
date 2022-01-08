78cfef29d5478539239e3f82d04c51d059a50fb6
Luis de la Torre <ldelatorre@dia.uned.es>
2022-01-08 17:42:43 +0100

    Update abis.js
    Removed ABIs for non-used tokens

--- packages/contracts/src/abis.js
+++ packages/contracts/src/abis.js
@@ -1,11 +1,7 @@
 import erc1155Abi from "./abis/erc1155.json";
-import erc20Abi from "./abis/erc20.json";
-import ownableAbi from "./abis/ownable.json";
 
 const abis = {
   erc1155: erc1155Abi,
-  erc20: erc20Abi,
-  ownable: ownableAbi,
 };
 
 export default abis;
